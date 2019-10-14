d52ed612546e241c4b4732fade81daab839c4d6a
Simon <greenspanx@hotmail.com>
2019-10-14 21:40:35 +0800

    first demo

---
+++ README.md
@@ -0,0 +1,9 @@
+
+## React Context
+
+Demo code to understand React Context
+
+
+## To Run
+
+1. do a `npm i` then a `npm start`
---
+++ src/App.js
@@ -0,0 +1,69 @@
+import React, { Component } from 'react';
+
+// first we will make a new context
+const MyContext = React.createContext();
+
+// Then create a provider Component
+class MyProvider extends Component {
+  state = {
+    name: 'Simon',
+    age: 30,
+    cool: true
+  }
+  render() {
+	// pass down context via value={} 
+    return (
+      <MyContext.Provider value={{
+        state: this.state,
+        growAYearOlder: () => this.setState({
+          age: this.state.age + 1
+        })
+      }}>
+	    {/*pass down children from parent components */}
+        {this.props.children}
+      </MyContext.Provider>
+    )
+  }
+}
+
+const Family = (props) => (
+  <div className="family">
+    <Person />
+  </div>
+)
+// Then create a Consumer Component
+class Person extends Component {
+  render() {
+    return (
+      <div className="person">
+        <MyContext.Consumer>
+		  {/*context comes from provider component value={}*/}
+          {(context) => (
+            <React.Fragment>
+              <p>Age: {context.state.age}</p>
+              <p>Name: {context.state.name}</p>
+              <button onClick={context.growAYearOlder}>getOlder</button>
+            </React.Fragment>
+          )}
+        </MyContext.Consumer>
+      </div>
+    )
+  }
+}
+
+
+class App extends Component {
+  render() {
+    return (
+      <MyProvider>
+        <div>
+          <p>I am the app</p>
+          <Family />
+        </div>
+      </MyProvider>
+    );
+  }
+}
+
+
+export default App;
